8ed48320370b10afabb7338056633d0ce1a3967e
Laura Espiño <126553407+LauraEspino@users.noreply.github.com>
2023-06-28 10:47:49 +0000

    return label y done

--- src/js/component/apptareas.jsx
+++ src/js/component/apptareas.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AppTareas = () => {
   // Declaración de estados
         const [tarea, setTarea] = useState("");
         const [listaTareas, setListaTareas] = useState([]);
+        const [user, setUser] = useState("");
 
     // Asignar evento on-change y vincular
         const addTarea = (e) => {
@@ -20,7 +21,38 @@ const AppTareas = () => {
             const newArr = listaTareas.filter((param, i) => i !== index);
             setListaTareas(newArr);
           };
-           
+        function createUser(){
+            fetch('https://assets.breatheco.de/apis/fake/todos/user/lauraespino',{
+                method: 'POST',
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify([])
+            })
+        
+           .then((response)=>[{label:tarea, done:false}])
+           .then((user)=>console.log(user))
+        //    .then((user)=>setUser(user.results))
+           .catch((error)=>console.log(error))
+        //    console.log(user)
+        }
+
+        function obtenerData(){
+            fetch('https://assets.breatheco.de/apis/fake/todos/user/lauraespino',{
+                method: 'GET',
+            })
+        
+           .then((response)=>response.json())
+           .then((data)=>console.log(data))
+        //    .then((user)=>setUser(user.results))
+           .catch((error)=>console.log(error))
+        //    console.log(user)
+        }
+        useEffect(()=>{
+            obtenerData()
+        },[])
+        
+
         return (
             <div className="container">
             <p className="">Lista de tareas</p>
@@ -32,13 +64,14 @@ const AppTareas = () => {
                 onKeyDown={eventEnter}
                 value={tarea}
             />
-            <div className="listaDeTareas">
+            <div className="listaDeTareas"> 
                 <ol className="ol list-group list-group-flush estiloLista">
-                  {listaTareas.map((task,index) => (
+                {[{label:listaTareas.map((task,index) => (
                     <li key={index} className="list-group-item li" >{`${task ==="" ? "No hay tareas pendientes" : task}`}<span className="span" onClick={() =>itemDelete(index)}>X</span></li>
-                    ))}
+                    )), done:false}]}
                 </ol>
                 <p className="tareasPendientes">{`${listaTareas.length ===0 ? "No item left" : listaTareas.length+" item left"}`}</p> 
+                <button className="btn btn-second" onClick={createUser}>Crear usuario</button>
             </div> 
             </div>
         );
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,12 +8,7 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<AppTareas/>
-			{/* <p className="tituloTodos">todos</p>
-			<ul className="list-group list-group-flush estiloLista">
-				<input placeholder="What needs to be done?"></input>
-				<li className="list-group-item">A second item <button type="button" class="btn btn-outline-light">X</button></li>
-				
-			</ul> */}
+			
 		</div>
 	);
 };
