f53793313995f8710b97d94e0bd12ac4f8201f9c
Laura Espiño <126553407+LauraEspino@users.noreply.github.com>
2023-06-29 19:48:28 +0000

    falta +1 tarea y delete

--- src/js/component/apptareas.jsx
+++ src/js/component/apptareas.jsx
@@ -13,16 +13,18 @@ const AppTareas = () => {
         const eventEnter = (e) =>{
             if(e.key === 'Enter') {
                  console.log(tarea)
-               setListaTareas(listaTareas.concat(tarea));
+               setListaTareas([...listaTareas,{label:tarea, done:false}]);
+               update()
                setTarea("")
                
         }}
         const itemDelete = (index) => {
             const newArr = listaTareas.filter((param, i) => i !== index);
             setListaTareas(newArr);
+            update()
           };
         function createUser(){
-            fetch('https://assets.breatheco.de/apis/fake/todos/user/lauraespino',{
+            fetch('https://assets.breatheco.de/apis/fake/todos/user/lauraesp',{
                 method: 'POST',
                 headers:{
                     "Content-Type": "application/json"
@@ -30,25 +32,37 @@ const AppTareas = () => {
                 body: JSON.stringify([])
             })
         
-           .then((response)=>[{label:tarea, done:false}])
-           .then((user)=>console.log(user))
-        //    .then((user)=>setUser(user.results))
+           .then((response)=>response.json())
+           .then((data)=>console.log(data))
            .catch((error)=>console.log(error))
-        //    console.log(user)
+        }
+
+        function update(){
+            fetch('https://assets.breatheco.de/apis/fake/todos/user/lauraesp',{
+                method: 'PUT',
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(listaTareas)
+            })
+        
+           .then((response)=>response.json())
+           .then((user)=>console.log(user))
+           .catch((error)=>console.log(error))
         }
 
         function obtenerData(){
-            fetch('https://assets.breatheco.de/apis/fake/todos/user/lauraespino',{
+            fetch('https://assets.breatheco.de/apis/fake/todos/user/lauraesp',{
                 method: 'GET',
             })
         
            .then((response)=>response.json())
-           .then((data)=>console.log(data))
-        //    .then((user)=>setUser(user.results))
+           .then((data)=>setListaTareas(data))
            .catch((error)=>console.log(error))
-        //    console.log(user)
         }
+       
         useEffect(()=>{
+            // createUser()
             obtenerData()
         },[])
         
@@ -66,9 +80,9 @@ const AppTareas = () => {
             />
             <div className="listaDeTareas"> 
                 <ol className="ol list-group list-group-flush estiloLista">
-                {[{label:listaTareas.map((task,index) => (
-                    <li key={index} className="list-group-item li" >{`${task ==="" ? "No hay tareas pendientes" : task}`}<span className="span" onClick={() =>itemDelete(index)}>X</span></li>
-                    )), done:false}]}
+                    {listaTareas.map((task,index) => (
+                    <li key={index} className="list-group-item li" >{`${task ==="" ? "No hay tareas pendientes" : task.label}`}<span className="span" onClick={() =>itemDelete(index)}>X</span></li>
+                    ))}
                 </ol>
                 <p className="tareasPendientes">{`${listaTareas.length ===0 ? "No item left" : listaTareas.length+" item left"}`}</p> 
                 <button className="btn btn-second" onClick={createUser}>Crear usuario</button>
@@ -77,4 +91,4 @@ const AppTareas = () => {
         );
         };
 
-export default AppTareas;
+export default AppTareas
